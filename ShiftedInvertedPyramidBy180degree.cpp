e7784fd8d3b402c63d417b3bb2e0d3d2bb5b06c3
Shivali Raju Bandi <40134659+ShivaliBandi@users.noreply.github.com>
2022-02-20 19:44:34 +0530

    Add files via upload

---
+++ ShiftedInvertedPyramidBy180degree.cpp
@@ -0,0 +1,32 @@
+/*
+* * * *
+  * * *
+    * *
+      * 
+*/
+#include<iostream>
+using namespace std;
+void pattern(int inumber)
+{
+    for(int i=0;i<inumber;i++)
+    {
+        for(int j=0;j<inumber;j++)
+        {
+                if(j>=i)
+                    cout<<"*";
+                else
+                    cout<<" ";
+            
+        }
+        
+        cout<<endl;
+    }
+}
+int main()
+{
+    int inumber=0;
+    cout<<"Enter inumber";
+    cin>>inumber;
+    pattern(inumber);
+    return 0;
+}
